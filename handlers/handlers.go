ad2ef6d59b10c02c76df9596a3b36bbfa549c50a
ulises <ulisescamps9@gmail.com>
2020-07-15 14:21:46 -0500

    fix handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+
 	"github.com/Neil-uli/tewto/middlew"
 	"github.com/Neil-uli/tewto/routers"
 	"github.com/gorilla/mux"
@@ -13,8 +14,7 @@ import (
 /*Handlers handl*/
 func Handlers() {
 	router := mux.NewRouter()
-	router.HandleFunc("/register", middlew.CheckBD(routers.Register)),Methods("POST")
-
+	router.HandleFunc("/register", middlew.CheckBD(routers.Register)).Methods("POST")
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = "8080"
